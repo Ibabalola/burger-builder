561382163210554b6d2973a732de6107fefcac44
Isaac Babalola <info@isaacbabalola.co.uk>
2020-01-07 18:00:58 +0000

    Initial Toolbar Skeleton

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 import styles from './Layout.module.css';
 
 export default ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={styles.Content}>
             {props.children}
         </main>
