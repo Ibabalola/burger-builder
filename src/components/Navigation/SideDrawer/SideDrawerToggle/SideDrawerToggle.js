7426c6c40d56057dec9aace6d9da229af779c0f4
Isaac Babalola <info@isaacbabalola.co.uk>
2020-01-13 23:56:02 +0000

    Adding CSS styling for side menu button toggle

--- src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
+++ src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
@@ -3,5 +3,9 @@ import React from 'react';
 import styles from './SideDrawerToggle.module.css';
 
 export default (props) => (
-    <div className={styles.SideDrawerToggle} onClick={props.clicked}>MENU</div>
+    <div className={styles.SideDrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
